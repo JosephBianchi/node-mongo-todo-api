2d663d06ee7aed1366e1445b89e3545bded600dd
Joe Bianchi <joseph.bianchi3@gmail.com>
2018-02-07 15:26:10 -0500

    added Get /todo:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +28,39 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    console.log('invalid id');
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      console.log('todo id not found');
+      return res.status(404).send()
+    }
+    return res.status(200).send({todo});
+  }, (e) => {
+    console.log('error');
+    res.status(400).send();
+  });
+
+
+  //valid id using is valid
+    // 404 - send back empty send
+
+  // findById look for matching document
+    //success
+      // if todo -send back
+      //if notodo - send back 404 with empty body
+    //error
+      //400 - send empty body back
+
+
 });
 
 app.listen(3000, () => {
